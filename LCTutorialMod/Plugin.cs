136246a73887453b9fdf38c9fca86f2802c2a4c3
Kyle Oakes <kyle.oakes2015@gmail.com>
2023-11-24 18:40:05 -0800

    Add project files.

---
+++ LCTutorialMod/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCTutorialMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TutorialModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "com."
+    }
+}
