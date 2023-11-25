52b83bf39286cc67c4cc0b30dda95ba251862eda
Kyle Oakes <kyle.oakes2015@gmail.com>
2023-11-24 20:47:15 -0800

    patch PlayerControllerB for infinite sprinting

---
+++ LCTutorialMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,21 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCTutorialMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")] // if Update() was public instead of private, we'd do [HarmonyPatch(nameof(PlayerControllerB.Update
+        [HarmonyPostfix] 
+        static void infiniteSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
--- LCTutorialMod/Plugin.cs
+++ LCTutorialMod/Plugin.cs
@@ -1,4 +1,7 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCTutorialMod.Patches;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,29 @@ namespace LCTutorialMod
     [BepInPlugin(modGUID, modName, modVersion)]
     public class TutorialModBase : BaseUnityPlugin
     {
-        private const string modGUID = "com."
+        private const string modGUID = "Technomancer113.LCTutorialMod";
+        private const string modName = "LC Tutorial";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TutorialModBase instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The test mod has awoken :)");
+
+            harmony.PatchAll(typeof(TutorialModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
     }
 }
